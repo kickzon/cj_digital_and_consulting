471214061e62433303c916893dc33015737eebe8
Augustlyden <august.lyden@outlook.com>
2026-04-20 16:59:54 +0200

    first attempt at fixing vite config for pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,14 @@
 import { defineConfig } from "vite";
+import { resolve } from 'path'
 
 export default defineConfig({
   base: "/lajc_digital_and_consulting/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        },
+    },
+  },
 });
